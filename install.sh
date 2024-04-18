fcb5c92456aa81496302094df849827c51417e0b
Curtis Wingert <cwingert@adventhp.com>
2024-04-18 18:54:58 +0000

    Copy files via GitHub Action

--- install.sh
+++ install.sh
@@ -1,5 +1,41 @@
 #!/bin/bash
 
+if [ -n "`$SHELL -c 'echo $ZSH_VERSION'`" ]; then
+   # assume Zsh
+   current_shell="$HOME/.zshrc"
+elif [ -n "`$SHELL -c 'echo $BASH_VERSION'`" ]; then
+   # assume Bash
+   current_shell="$HOME/.bashrc"
+else
+  echo "Unable to determine the default shell profile file."
+  exit 1
+fi
+
+# Function to prompt user for variable if it doesn't exist in the shell file
+prompt_variable() {
+    local variable_name="$1"
+    local shell_profile="$2"
+
+    # Check if the variable is already defined in the shell file
+    if ! grep -q "^export $variable_name=" "$shell_profile"; then
+        while true; do
+            read -p "Enter $variable_name: " value
+            if [ -z "$value" ]; then
+                echo "Error: $variable_name cannot be empty."
+            else
+                echo "export $variable_name=\"$value\"" >> "$shell_profile"
+                break
+            fi
+        done
+    fi
+}
+
+# Check and set environment variables
+prompt_variable "UHC_CLIENT_ID" "$current_shell"
+prompt_variable "UHC_CLIENT_SECRET" "$current_shell"
+prompt_variable "CAVO_API_USERNAME" "$current_shell"
+prompt_variable "CAVO_API_PASSWORD" "$current_shell"
+
 # Prompt the user for the operating system architecture
 while true; do
     echo "Select your operating system architecture:"
@@ -13,34 +49,21 @@ while true; do
         1)
             # macOS (Intel)
             BINARY_PATH="amd/uhchutil"
-            SHELL_PROFILE=~/.zshrc
             break
             ;;
         2)
             # macOS (Apple Silicon)
             BINARY_PATH="arm/uhchutil"
-            SHELL_PROFILE=~/.zshrc
             break
             ;;
         3)
             # Linux
             BINARY_PATH="linux/uhchutil"
-            if [ -n "$ZSH_VERSION" ]; then
-                # Zsh is the default shell
-                SHELL_PROFILE=~/.zshrc
-            elif [ -n "$BASH_VERSION" ]; then
-                # Bash is the default shell
-                SHELL_PROFILE=~/.bashrc
-            else
-                echo "Unable to determine the default shell. Please manually update your shell profile file."
-                exit 1
-            fi
             break
             ;;
         4)
             # Windows
             BINARY_PATH="win/uhchutil"
-            # No shell profile update needed for Windows
             break
             ;;
         *)
@@ -59,16 +82,23 @@ fi
 GITHUB_DOWNLOAD_PATH="https://github.com/cpwingert/uhcutil-deploy/raw/main/bin"
 
 # Prompt the user for the installation path
-read -p "Enter the installation path (e.g., /usr/local/bin): " INSTALL_PATH
-
-# Check if the installation path exists, if not, create it
-if [ ! -d "$INSTALL_PATH" ]; then
-    mkdir -p "$INSTALL_PATH"
-    echo "Directory '$INSTALL_PATH' created."
-fi
-
+while true; do
+    read -p "Enter the installation path (e.g., /usr/local/bin): " INSTALL_PATH
+    if [ -z "$INSTALL_PATH" ]; then
+        echo "Error: Installation path cannot be empty."
+    elif [ ! -d "$INSTALL_PATH" ]; then
+        mkdir -p "$INSTALL_PATH"
+        echo "Directory '$INSTALL_PATH' created."
+        break
+    else
+        break
+    fi
+done
 # Download the binary from GitHub
-curl -L -o "$INSTALL_PATH/uhcutil" "$GITHUB_DOWNLOAD_PATH/$BINARY_PATH"
+if ! curl -L -o "$INSTALL_PATH/uhcutil" "$GITHUB_DOWNLOAD_PATH/$BINARY_PATH"; then
+    echo "Error: Failed to download the binary. Installation aborted."
+    exit 1
+fi
 
 # Change the permissions of the binary to executable
 chmod +x "$INSTALL_PATH/uhcutil"
@@ -77,10 +107,10 @@ chmod +x "$INSTALL_PATH/uhcutil"
 if ! echo "$PATH" | grep -q "$INSTALL_PATH"; then
 
     # Update the shell profile file based on the selected architecture
-    echo '' >> "$SHELL_PROFILE"
-    echo 'export PATH="$PATH:'"$INSTALL_PATH"'"' >> "$SHELL_PROFILE"
+    echo '' >> "$current_shell"
+    echo 'export PATH="$PATH:'"$INSTALL_PATH"'"' >> "$current_shell"
 
-    echo "Please restart your shell or run 'source $SHELL_PROFILE' to apply changes."
+    echo "Please restart your shell or run 'source $current_shell' to apply changes."
 else
     echo "The installation path '$INSTALL_PATH' is already included in the PATH variable. No changes made to shell profile files."
 fi
